ce95b2cbd8b0283bc79fea3e0289809a57ad01b0
Steven <chaolic6505@gmail.com>
2024-07-17 11:35:27 -0700

    fix sidebar style

--- components/admin-panel/menu.tsx
+++ components/admin-panel/menu.tsx
@@ -25,8 +25,8 @@ export function Menu({ isOpen }: MenuProps) {
     const menuList = getMenuList(pathname);
 
     return (
-        <ScrollArea className="[&>div>div[style]]:!block">
-            <nav className="mt-8 h-full w-full">
+        <ScrollArea className="[&>div>div[style]]:!block overflow-y-auto">
+            <nav className="mt-0 h-full w-full">
                 <ul className="flex flex-col min-h-[calc(100vh-48px-36px-16px-32px)] lg:min-h-[calc(100vh-32px-40px-32px)] items-start space-y-1 px-2">
                     {menuList.map(({ groupLabel, menus }, index) => (
                         <li className={cn("w-full", groupLabel ? "pt-5" : "")} key={index}>
