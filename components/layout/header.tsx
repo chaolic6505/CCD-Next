98b68ff770af5f9f24554ed1c823d2acec8fb8cd
Steven <chaolic6505@gmail.com>
2024-07-19 19:20:20 -0700

    update

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,17 +3,18 @@ import { SheetMenu } from "@/components/admin-panel/sheet-menu";
 import ThemeToggle from "@/components/layout/ThemeToggle/theme-toggle";
 
 export default function Header() {
-  return (
-    <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
-      <div className="mx-4 sm:mx-8 flex h-14 items-center">
-        <div className="flex items-center space-x-4 lg:space-x-0">
-          <SheetMenu />
-        </div>
-        <div className="flex flex-1 items-center space-x-2 justify-end">
-          <ThemeToggle />
-          <UserNav />
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
+            <div className="mx-4 sm:mx-8 flex h-14 items-center">
+                <div className="flex items-center space-x-4 lg:space-x-0">
+                    <ThemeToggle />
+
+                </div>
+                <div className="flex flex-1 items-center justify-end space-x-5 ">
+                    <UserNav />
+                    <SheetMenu />
+                </div>
+            </div>
+        </header>
+    );
 }
