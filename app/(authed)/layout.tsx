938d13695e44c6f8ee5d7745aec6fa9a6124feed
Steven <chaolic6505@gmail.com>
2024-07-23 21:04:05 -0700

    update Clerk provider import

--- app/(authed)/layout.tsx
+++ app/(authed)/layout.tsx
@@ -2,6 +2,7 @@ import { AI } from "../actions";
 import Header from "@/components/layout/header";
 import Transition from "@/components/Transition";
 import { RouteChangeHandler } from "@/components/RouteChangeHandler";
+
 export default function Page({ children }: { children: React.ReactNode }) {
     return (
         <Transition>
