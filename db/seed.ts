1311adad047e473c5298eda4ba3a4545320280df
Steven <chaolic6505@gmail.com>
2024-08-04 11:03:25 -0700

    add drizzle and connect to neon

---
+++ db/seed.ts
@@ -0,0 +1,31 @@
+import { exit } from "process";
+
+import { db } from "./drizzle";
+import * as schema from "./schema";
+
+import "@/db/env-config";
+import { customers, invoices, revenue, users } from "@/lib/placeholder-data";
+
+const main = async () => {
+    try {
+        await db.transaction(async (tx) => {
+            await tx.delete(schema.revenue);
+            await tx.delete(schema.invoices);
+            await tx.delete(schema.customers);
+            await tx.delete(schema.users);
+
+            await tx.insert(schema.users).values(users);
+            await tx.insert(schema.customers).values(customers);
+            await tx.insert(schema.invoices).values(invoices);
+            await tx.insert(schema.revenue).values(revenue);
+        });
+
+        console.log("Database seeded successfully");
+        exit(0);
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to seed database");
+    }
+};
+
+main();
