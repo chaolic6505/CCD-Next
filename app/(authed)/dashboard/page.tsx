63a7ba5dff7905879af8db2482d8857c530ec9a7
Steven <chaolic6505@gmail.com>
2024-08-15 23:10:48 -0700

    internationalization work

--- app/(authed)/dashboard/page.tsx
+++ app/(authed)/dashboard/page.tsx
@@ -1,39 +1,50 @@
 import { Suspense } from "react";
+import { useTranslations } from 'next-intl';
 
 import {
     CardsSkeleton,
     RevenueChartSkeleton,
     LatestInvoicesSkeleton,
 } from "@/components/shared/skeletons";
-import { CalendarDateRangePicker } from "@/components/date-range-picker";
-import { Button } from "@/components/ui/button";
-import { BarGraph } from "@/components/charts/bar-graph";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
 import DashboardCardsWrapper from "./(shared)/dashboard-card-wrapper";
 import LatestInvoicesWrapper from "./(shared)/latest-invoices-wrapper";
-import { AreaGraph } from "@/components/charts/area-graph";
-import { PieGraph } from "@/components/charts/pie-graph";
 
-export default function page() {
+import { Button } from "@/components/ui/button";
+import { PieGraph } from "@/components/charts/pie-graph";
+import { BarGraph } from "@/components/charts/bar-graph";
+import { AreaGraph } from "@/components/charts/area-graph";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { CalendarDateRangePicker } from "@/components/date-range-picker";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+
+
+
+
+export default function Dashboard() {
+    const t = useTranslations('dashboard');
+
     return (
         <ScrollArea className="h-full overflow-hidden mb-10">
             <div className="flex-1 space-y-4 p-4 md:p-8 mb-10">
                 <div className="flex items-center justify-between space-y-2">
                     <h2 className="text-3xl font-bold tracking-tight">
-                        Hi, Welcome back 👋
+                        {t('title')} 👋
                     </h2>
                     <div className="hidden items-center space-x-2 md:flex">
                         <CalendarDateRangePicker />
-                        <Button>Download</Button>
+                        <Button>
+                            {t('download')}
+                        </Button>
                     </div>
                 </div>
                 <Tabs defaultValue="overview" className="space-y-4">
                     <TabsList>
-                        <TabsTrigger value="overview">Overview</TabsTrigger>
+                        <TabsTrigger value="overview">
+                            {t('overview_tab')}
+                        </TabsTrigger>
                         <TabsTrigger value="analytics" disabled>
-                            Analytics
+                            {t('analytics_tab')}
                         </TabsTrigger>
                     </TabsList>
                     <TabsContent value="overview" className="space-y-4">
